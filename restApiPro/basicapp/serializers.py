5c459fee5d22f6ebdd62b18e38907d3d57cefb3d
Jassi <jaswinder.dandyan2000@gmail.com>
2020-08-17 14:44:13 +0530

    Added profile viewsets

--- restApiPro/basicapp/serializers.py
+++ restApiPro/basicapp/serializers.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
 
-
+from basicapp import  models
 class HelloSerializers(serializers.Serializer): 
     """Serializiers a name field for testing API view"""
 
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializer a user profile object """
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password' : {
+                'write_only' : True,
+                'style' : {'input_type' : 'password'}
+            }
+        }
+
+    def create(self,validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )    
+        
+        return user
--- restApiPro/basicapp/urls.py
+++ restApiPro/basicapp/urls.py
@@ -5,7 +5,7 @@ from basicapp import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='helloviewset')
-
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('api/',views.HelloApiView.as_view(),name='api'),
