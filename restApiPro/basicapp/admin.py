f2713ca4e0d0d4fe1ee24519c731b53bcce0798f
Jassi <jaswinder.dandyan2000@gmail.com>
2020-08-18 11:33:53 +0530

    login api added

--- restApiPro/basicapp/admin.py
+++ restApiPro/basicapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+from basicapp import models
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
--- restApiPro/basicapp/urls.py
+++ restApiPro/basicapp/urls.py
@@ -6,6 +6,7 @@ from basicapp import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='helloviewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('api/',views.HelloApiView.as_view(),name='api'),
